96a1740724dca4b7d7a9e94db723dcb14f8e040c
zhangChunLong <158098347@qq.com>
2020-11-11 18:07:06 +0800

    修改1

---
+++ src/test.java
@@ -0,0 +1,13 @@
+/**
+ * @author zcl
+ * @version 1.0
+ * @ClassName test
+ * @Description 一句话描述此类或方法的作用
+ * @Date 2020/11/11 18:06
+ */
+public class test {
+
+    public void add(){
+        System.out.println("nihao a");
+    }
+}
